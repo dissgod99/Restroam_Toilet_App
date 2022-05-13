c007fd2f4137de7a31c4f121ce59afd84946af88
aziz <aziz.chikh@stud.tu-darmstadt.de>
2022-05-13 12:38:24 +0200

    Made Rating page scrollbar, changed the design of TextInput and fixed max length input by description

--- projectJ/src/screens/RateToilet/RatingToiletScreen.js
+++ projectJ/src/screens/RateToilet/RatingToiletScreen.js
@@ -1,16 +1,21 @@
 import React, {useState, Component} from "react";
-import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity, ScrollView } from "react-native";
+import { View, Text, Button, StyleSheet, TouchableOpacity, ScrollView, SafeAreaView } from "react-native";
 import * as Animatable from 'react-native-animatable';
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import StarRating from 'react-native-star-rating';
+import { TextInput } from 'react-native-paper';
 
 const RatingToiletScreen = ({ navigation }) => {
 
+    // Stars section
     const defaultRating = 3;
     const maxRating = 5; 
     const [starCount1, setStarCount1] = useState(defaultRating)
     const [starCount2, setStarCount2] = useState(defaultRating)
     const [starCount3, setStarCount3] = useState(defaultRating)
+
+    //Description section
+    const [text, setText] = useState('');
 
     const onStarRatingPress = (rating) => {
         setStarCount1(rating)
@@ -32,78 +37,83 @@ const RatingToiletScreen = ({ navigation }) => {
     const total = parseFloat(((starCount1 + starCount2 + starCount3)/3).toFixed(2));
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.rateToilet}>Rate Toilet : </Text> 
-            <View style={styles.center}>
-                <Text style={styles.smallMargin}>
-                    Cleaness {starCount1} / {maxRating}
-                </Text>
-                <StarRating 
-                    maxStars={maxRating}
-                    disabled={false}
-                    rating={starCount1}
-                    selectedStar={(rating) => onStarRatingPress(rating)}
-                    fullStarColor={"gold"}
+        <SafeAreaView >
+        <ScrollView >
+            <View style={styles.container}>
+                <Text style={styles.rateToilet}>Rate Toilet : </Text> 
+                <View style={styles.center}>
+                    <Text style={styles.smallMargin}>
+                        Cleaness {starCount1} / {maxRating}
+                    </Text>
+                    <StarRating 
+                        maxStars={maxRating}
+                        disabled={false}
+                        rating={starCount1}
+                        selectedStar={(rating) => onStarRatingPress(rating)}
+                        fullStarColor={"gold"}
+                    />
+                </View>
+
+                <View style={styles.center}>
+                    <Text style={styles.smallMargin}>
+                        Waiting time {starCount2} / {maxRating}
+                    </Text>
+                    <StarRating 
+                        maxStars={maxRating}
+                        disabled={false}
+                        rating={starCount2}
+                        selectedStar={(rating) => onStarRatingPress_wt(rating)}
+                        fullStarColor={"gold"}
+                    />
+                </View>
+                <View style={styles.center}>
+                    <Text style={styles.smallMargin}>
+                        Security {starCount3} / {maxRating}
+                    </Text>
+                    <StarRating 
+                        maxStars={maxRating}
+                        disabled={false}
+                        rating={starCount3}
+                        selectedStar={(rating) => onStarRatingPress_sec(rating)}
+                        fullStarColor={"gold"}
+                    />
+                </View>
+
+                <View style={styles.center}>
+                    <Text style={styles.smallMargin}>
+                        Total {total} / {maxRating}
+                    </Text>
+                    <StarRating 
+                        maxStars={maxRating}
+                        disabled={true}
+                        rating={total}
+                        fullStarColor={"gold"}
+                    />
+                </View>
+
+                <View style={styles.description}>
+                    
+                <TextInput style={styles.box}
+                    mode="outlined"
+                    label="Description"
+                    placeholder="Type something"
+                    right={<TextInput.Affix text="/250" />}
+                    activeOutlineColor="#e6697e"
                 />
+                </View>
+                <TouchableOpacity 
+                    style={styles.btn}
+                    onPress={handleSubmit}
+                    >
+                    <Text style={styles.stOfSubmit}>
+                            Submit
+
+                    </Text>
+                </TouchableOpacity>
             </View>
 
-            <View style={styles.center}>
-                <Text style={styles.smallMargin}>
-                    Waiting time {starCount2} / {maxRating}
-                </Text>
-                <StarRating 
-                    maxStars={maxRating}
-                    disabled={false}
-                    rating={starCount2}
-                    selectedStar={(rating) => onStarRatingPress_wt(rating)}
-                    fullStarColor={"gold"}
-                />
-            </View>
-            <View style={styles.center}>
-                <Text style={styles.smallMargin}>
-                    Security {starCount3} / {maxRating}
-                </Text>
-                <StarRating 
-                    maxStars={maxRating}
-                    disabled={false}
-                    rating={starCount3}
-                    selectedStar={(rating) => onStarRatingPress_sec(rating)}
-                    fullStarColor={"gold"}
-                />
-            </View>
-
-            <View style={styles.center}>
-                <Text style={styles.smallMargin}>
-                    Total {total} / {maxRating}
-                </Text>
-                <StarRating 
-                    maxStars={maxRating}
-                    disabled={true}
-                    rating={total}
-                    fullStarColor={"gold"}
-                />
-            </View>
-
-
-            <View style={styles.description}>
-                <Text style={styles.descriptionTxt}>Description (Optional)</Text>
-            <TextInput style={styles.box}
-                title="Enter Description"
-            
-            />
-            </View>
-
-
-            <TouchableOpacity 
-                style={styles.btn}
-                onPress={handleSubmit}
-                >
-                <Text style={styles.stOfSubmit}>
-                        Submit
-
-                </Text>
-            </TouchableOpacity>
-        </View>      
+        </ScrollView>
+        </SafeAreaView>      
     )
 }
 
@@ -142,7 +152,7 @@ const styles = StyleSheet.create({
         fontWeight: "bold"
     },
     box:{
-        backgroundColor: "#ded8d9",
+        //backgroundColor: "#ded8d9",
         width: 250,
         //marginVertical: 10,
         paddingLeft: 8,
